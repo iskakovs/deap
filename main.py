f480ac5ae06048c02abaf5b24f909e1bfa2bbd85
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-29 12:36:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+# The "DEAP" package documentation: https://deap.readthedocs.io/en/master/index.html
+
+# Loading the packages
+from deap import base, algorithms # "base" module provides basic structures to build evolutionary algorithms: https://deap.readthedocs.io/en/master/api/base.html
 
