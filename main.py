e7885a2a3ff3df9e200de82ccbbfd27405c973d9
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-29 12:45:19 +0600

    Import 'tools' from DEAP

--- main.py
+++ main.py
@@ -4,4 +4,4 @@
 from deap import base, algorithms # "base" module provides basic structures to build evolutionary algorithms: https://deap.readthedocs.io/en/master/api/base.html, 
 # to learn more about algorithms: https://deap.readthedocs.io/en/master/tutorials/basic/part2.html#algorithms
 from deap import creator # The creator is a meta-factory allowing to create classes that will fulfill the needs of your evolutionary algorithms: https://deap.readthedocs.io/en/master/api/creator.html?highlight=creator
-
+from deap import tools
