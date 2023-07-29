eecab8dcdff855aadec2e497fae71c72f85aa448
Serik Iskakov <103096336+iskakovs@users.noreply.github.com>
2023-07-29 12:40:26 +0600

    Continue listing the needed packages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # The "DEAP" package documentation: https://deap.readthedocs.io/en/master/index.html
 
 # Loading the packages
-from deap import base, algorithms # "base" module provides basic structures to build evolutionary algorithms: https://deap.readthedocs.io/en/master/api/base.html
+from deap import base, algorithms # "base" module provides basic structures to build evolutionary algorithms: https://deap.readthedocs.io/en/master/api/base.html, 
+# to learn more about algorithms: https://deap.readthedocs.io/en/master/tutorials/basic/part2.html#algorithms
 
